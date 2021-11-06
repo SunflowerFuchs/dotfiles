8811741f0a571a478befa164d081067555f957f2
Pascal Pirschel <pascal.pirschel@gmail.com>
2021-11-06 21:32:07 +0100

    Made stuff compatible with non-admins

--- install.sh
+++ install.sh
@@ -3,19 +3,33 @@
 preInstall() {
     # make sure all important variables are filled
     dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    USER="${USER:-$(whoami)}"
-    HOME="${HOME:-~$USER}"
-    SUDO="sudo"
 
-    # if this script gets run as root, get the original user
-    if [[ $(id -ur) -eq 0 ]] && [[ ! -z ${SUDO_USER} ]]; then
+    if [[ -z "${USER}" ]]; then
+        USER="$(whoami)"
+    fi
+
+    if [[ -z "${HOME}" ]]; then
+        HOME="$( getent passwd "${USER}" | cut -d: -f6 )"
+    fi
+
+    SUDO=""
+    IS_ADMIN=false
+    if [[ ! -z ${SUDO_USER} ]]; then
+        # if this script gets run with sudo as root, get the original user
         USER="${SUDO_USER}"
         # check that the user exists before we eval
         if ! getent passwd ${USER} > /dev/null 2>&1; then
             exit 1
         fi
         HOME=$(eval echo "~${USER}")
-        SUDO=""
+        IS_ADMIN=true
+    elif groups | grep --word-regexp 'sudo' >/dev/null 2>&1; then
+        # user is part of sudoers
+        IS_ADMIN=true
+        SUDO="sudo"
+    elif [[ $(id -ur) -eq 0 ]]; then
+        # root
+        IS_ADMIN=true
     fi
 
     # figure out which package manager to use
@@ -43,6 +57,12 @@ preInstall() {
 }
 
 runUpdates() {
+    if [[ ! "$IS_ADMIN" = true ]]; then
+        # Not an admin, cannot install updates
+        # Not a problem tho, we can just quietly return
+        return
+    fi
+
     echo "Running updates..."
     if [[ -x "$(command -v brew)" ]]; then
         brew update > /dev/null 2>&1
@@ -70,6 +90,10 @@ setupDirs() {
         mkdir "${LOCAL}"
     fi
 
+    if [[ ! -d "${HOME}/.config/" ]]; then
+        mkdir "${HOME}/.config/"
+    fi
+
     if [[ ! -d "${LOCAL}/bin/" ]]; then
         mkdir "${LOCAL}/bin/"
     fi
@@ -77,6 +101,14 @@ setupDirs() {
 
 getCurl() {
     if [[ ! -x "$(command -v curl)" ]]; then
+        if [[ ! "$IS_ADMIN" = true ]]; then
+            # Not an admin, cannot install curl
+            # This is a problem, because many of the things we download need curl
+            # TODO: check wget as an alternative
+            echo "No curl installed and no permissions to install it, exiting..." >&2
+            exit 1
+        fi
+
         echo "Installing curl..."
         ${INSTALL} curl > /dev/null 2>&1
         if [ $? -ne 0 ]; then
@@ -88,6 +120,14 @@ getCurl() {
 
 getTmux() {
     if [[ ! -x "$(command -v tmux)" ]]; then
+        if [[ ! "$IS_ADMIN" = true ]]; then
+            # Not an admin, cannot install tmux
+            # This sucks, but at least it's not gonna break anything
+            # TODO: disable tmux plugin in zsh
+            return
+        fi
+
+
         echo "Installing tmux..."
         ${INSTALL} tmux > /dev/null 2>&1
         if [ $? -ne 0 ]; then
@@ -105,7 +145,7 @@ getMicro() {
 
     if [[ ! -x "$(command -v micro)" ]]; then
         echo "Installing micro..."
-        if [[ -x "$(command -v nix-env)" ]]; then
+        if [[ -x "$(command -v nix-env)" ]] && [[ "$IS_ADMIN" = true ]]; then
             # luckily, nix has a pre-build micro package
             ${INSTALL} micro > /dev/null 2>&1
             if [ $? -ne 0 ]; then
@@ -119,13 +159,21 @@ getMicro() {
             chmod u+x /tmp/install.sh
             /tmp/install.sh > /dev/null 2>&1
             rm /tmp/install.sh
-            mv /tmp/micro "${HOME}/.local/bin/micro"
+
+            if [[ "$IS_ADMIN" != true ]] || [[ ! -d /usr/local/bin/ ]]; then
+                # Not an admin (or bin dir not available), install into user bin dir
+                mv /tmp/micro "${HOME}/.local/bin/micro"
+            else
+                ${SUDO} mv /tmp/micro /usr/local/bin/micro
+            fi
+
             cd "${dotfiles}"
         fi
     fi
 
     # On mac we don't need to install xsel
-    if [ "$(uname)" != "Darwin" ] && [[ ! -x "$(command -v xsel)" ]]; then
+    # Also doesn't get installed when no permissions, but thats no problem
+    if [ "$(uname)" != "Darwin" ] && [[ ! -x "$(command -v xsel)" ]] && [[ "$IS_ADMIN" = true ]]; then
         echo "Installing xsel for micro..."
 
         # on nix, the package is called xsel-unstable instead
@@ -149,6 +197,12 @@ getZsh() {
     getCurl
 
     if [[ ! -x "$(command -v zsh)" ]]; then
+        if [[ ! "$IS_ADMIN" = true ]]; then
+            # Not an admin, cannot install zsh
+            # This sucks big time, but it's survivable
+            return
+        fi
+
         echo "Installing zsh..."
         ${INSTALL} zsh > /dev/null 2>&1
         if [ $? -ne 0 ]; then
@@ -157,10 +211,11 @@ getZsh() {
         fi
 
         if [[ -x "$(command -v chsh)" ]]; then
-            ${SUDO} chsh -s "$(command -v zsh)" "${USER}"
+            echo "Changing default shell..."
+            chsh -s "$(command -v zsh)" "${USER}"
         fi
 
-        if [[ ! -x "$(command -v chsh)" ]] || [[ $? -ne 0 ]]; then
+        if [[ ! -x "$(command -v chsh)" ]] || [[ ! "$( getent passwd "${USER}" | cut -d: -f7 )" = "$(command -v zsh)" ]]; then
             echo "Could not change default shell. Good luck."
         fi
     fi
@@ -187,14 +242,14 @@ getZsh() {
     # installing the theme
     mkdir -p "${ZSH_CUSTOM}/themes/"
     if [[ ! -d "${ZSH_CUSTOM}/themes/spaceship-prompt" ]]; then
-        git clone https://github.com/denysdovhan/spaceship-prompt.git "${ZSH_CUSTOM}/themes/spaceship-prompt" > /dev/null
+        git clone --quiet https://github.com/denysdovhan/spaceship-prompt.git "${ZSH_CUSTOM}/themes/spaceship-prompt" > /dev/null
         ln -s "${ZSH_CUSTOM}/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM}/themes/spaceship.zsh-theme"
     fi
 
     # adding plugins
     mkdir -p "${ZSH_CUSTOM}/plugins/"
     if [[ ! -d "${ZSH_CUSTOM}/plugins/zsh-completions" ]]; then
-        git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM}/plugins/zsh-completions" > /dev/null
+        git clone --quiet https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM}/plugins/zsh-completions" > /dev/null
     fi
 
     if [[ ! -d "${ZSH_CUSTOM}/plugins/ve" ]] && [[ -x "$(command -v virtualenv)" ]]; then
@@ -210,17 +265,17 @@ getZsh() {
     fi
 
     if [[ ! -d "${ZSH_CUSTOM}/plugins/fast-syntax-highlighting" ]]; then
-        git clone https://github.com/zdharma/fast-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/fast-syntax-highlighting" > /dev/null
+        git clone --quiet https://github.com/zdharma-continuum/fast-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/fast-syntax-highlighting" > /dev/null
         cp "${dotfiles}/.zsh/custom/fast-syntax-highlighting.theme" "${ZSH_CUSTOM}/plugins/fast-syntax-highlighting/themes/custom.ini"
     fi
 
     if [[ ! -d "${ZSH_CUSTOM}/plugins/nix-zsh-completions" ]] && [[ -d "/nix" ]]; then
-        git clone https://github.com/spwhitt/nix-zsh-completions.git "${ZSH_CUSTOM}/plugins/nix-zsh-completions" > /dev/null
+        git clone --quiet https://github.com/spwhitt/nix-zsh-completions.git "${ZSH_CUSTOM}/plugins/nix-zsh-completions" > /dev/null
     fi
 
     if [[ ! -x "$(command -v fzf)" ]]; then
         echo "Installing fzf..."
-        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf > /dev/null
+        git clone --quiet --depth 1 https://github.com/junegunn/fzf.git ~/.fzf > /dev/null
         "${HOME}/.fzf/install" --no-update-rc --key-bindings --completion --no-bash --no-fish > /dev/null
         mv "${HOME}/.fzf.zsh" "${ZDOTDIR}/"
         mv "${HOME}/.fzf/bin/fzf" "${HOME}/.local/bin"
